0bc6d4ee8ca8511f8a9f38e3287068fce3fd471e
Carlos Hermosillo <hermcode@gmail.com>
2023-01-19 19:58:05 -0600

    Steps component added to barrel

--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export * from './Category';
 export { default as Product } from './Product';
 export * from './Sidebar';
 export { default as ProductModal } from './ProductModal';
+export { default as Steps } from './Steps';
