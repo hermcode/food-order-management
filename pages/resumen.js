eff24b5d2b9b6b00ac4cdd81bddf4cfa63f3ae70
Carlos Hermosillo <hermcode@gmail.com>
2023-01-23 14:59:01 -0600

    resumen page finished along with it ProductOverview component

--- pages/resumen.js
+++ pages/resumen.js
@@ -1,10 +1,28 @@
 import Layout from "../layout/layout"
+import useFOM from "../hooks/useFOM"
+import { ProductOverview } from "../components"
 
 export default function Resumen() {
+
+  const { order } = useFOM()
+
   return (
     <Layout page='Resúmen'>
-      <h1 className="text-4xl font-bold text-green uppercase mt-3">Summary</h1>
+      <h1 className="text-4xl font-bold text-green uppercase mt-3">Resúmen</h1>
       <p className="text-2xl my-5 font-bold">Revisa tu pedido</p>
+
+      {
+        order.length === 0 ? (
+          <p className="text-center text-2xl">No hay elementos en tu carrito</p>
+        ) : (
+          order.map( product => 
+            <ProductOverview 
+              key={product.id}
+              product={product}
+            />
+          )
+        )
+      }
     </Layout>
   )
 }
