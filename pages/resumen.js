b2b400fdedf2ff2a66347c790a442f5bff741c9e
Carlos Hermosillo <hermcode@gmail.com>
2023-01-23 10:03:00 -0600

    summary.js name changed to resumen.js

--- pages/resumen.js
+++ pages/resumen.js
@@ -1,8 +1,8 @@
 import Layout from "../layout/layout"
 
-export default function Summary() {
+export default function Resumen() {
   return (
-    <Layout page='Summary'>
+    <Layout page='Resúmen'>
       <h1 className="text-4xl font-bold text-green uppercase mt-3">Summary</h1>
       <p className="text-2xl my-5 font-bold">Revisa tu pedido</p>
     </Layout>
